3ec47024cdea607f9cda88dadec56024334fb196
karthika <131744649+Karthikaarul@users.noreply.github.com>
2024-03-29 19:20:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template, redirect, flash, send_file
+from sklearn.preprocessing import MinMaxScaler
+from werkzeug.utils import secure_filename
+import pickle
+
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template, redirect, flash, send_file
+from sklearn.preprocessing import MinMaxScaler
+from werkzeug.utils import secure_filename
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+app = Flask(__name__) #Initialize the flask App
+
+
+decisions = pickle.load(open('model.pkl','rb'))
+@app.route('/')
+
+@app.route('/first')
+def first():
+	return render_template('first.html')
+
+@app.route('/chart')
+def chart():
+	return render_template('chart.html')
+
+#@app.route('/future')
+#def future():
+#	return render_template('future.html')    
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')  
+@app.route('/preview',methods=["POST"])
+def preview():
+    if request.method == 'POST':
+        dataset = request.files['datasetfile']
+        df = pd.read_csv(dataset,encoding = 'unicode_escape')
+        df.set_index('land', inplace=True)
+        return render_template("preview.html",df_view = df)	
+
+
+#@app.route('/home')
+#def home():
+ #   return render_template('home.html')
+
+@app.route('/prediction', methods = ['GET', 'POST'])
+def prediction():
+    return render_template('prediction.html')
+
+
+#@app.route('/upload')
+#def upload_file():
+#   return render_template('BatchPredict.html')
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	int_feature = [x for x in request.form.values()]
+	 
+	final_features = [np.array(int_feature)]
+	 
+	result=decisions.predict(final_features)
+	print(result)
+	if result == 1:
+			result = "network is attacked"
+	else:
+		result = 'network is not attacked'
+	
+	return render_template('prediction.html', prediction_text= result)
+@app.route('/performance')
+def performance():
+	return render_template('performance.html')   
+    
+if __name__ == "__main__":
+    app.run(debug=True)
